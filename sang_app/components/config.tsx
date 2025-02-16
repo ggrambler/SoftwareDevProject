ac16525c4f1c8b58c2dd396ad19c40a422a2d2cf
ggrambler <jha02032003@gmail.com>
2025-02-16 13:56:43 +0530

    second changes, use initial commit to run

---
+++ sang_app/components/config.tsx
@@ -0,0 +1,10 @@
+
+export const config = {
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
+  NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID || "",
+  SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET || "",
+  YOUTUBE_API: process.env.YOUTUBE_API || "",
+};
